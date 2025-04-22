72050f228d7e87d9fca4da1e707725d48da3b767
Carlos Charles <sengicarloscharles@gmail.com>
2025-04-23 01:55:47 +0300

    added php in Login System Project

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
 </head>
 <body>
 
-    <form action="">
-        <h2>Login</h2>
+    <form action="login.php" method="post">
+        <h1>Login</h1>
+        <?php if (isset($_GET['error'])){ ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+       <?php };?>
         <label for="">User name</label>
-        <input type="text" name="uname" placeholder="User Name">
+        <input type="text" name="uname" placeholder="User Name"><br>
 
         <label for="">Password</label>
-        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="password" placeholder="Password"><br>
 
         <button type="submit">Login</button>
     </form>
